c750c1734e99cbf5c5cac00d59fa38f9ec332985
Vivek.Hattarge <Vivek.Hattarge@gmail.com>
2021-03-28 14:11:53 +0530

    What:
    1. Program to find the length of the longest substring, which has no repeating characters.

---
+++ Data Structure/src/com/miscellaneous/NoRepeatingLongestSubString.java
@@ -0,0 +1,33 @@
+package com.miscellaneous;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NoRepeatingLongestSubString {
+
+    public static void main(String[] args) {
+
+        System.out.println("Longest non repeating substring count is : "+ getLongestNonRepeatingSubstringCount("aabccbb"));
+
+    }
+//aabccbb
+    private static int getLongestNonRepeatingSubstringCount(String inputString) {
+        int windowStart= 0;
+        int maxCount=0;
+        Set<Character> uniqueCharacters = new HashSet<>();
+        for(int windowEnd=0; windowEnd < inputString.length();windowEnd++){
+
+            char leftChar = inputString.charAt(windowEnd);
+            if (uniqueCharacters.contains(leftChar)) {
+                maxCount = Math.max(maxCount, (windowEnd-1) - windowStart + 1);
+                windowStart = windowEnd;
+                uniqueCharacters.clear();
+            } else {
+                uniqueCharacters.add(leftChar);
+            }
+
+        }
+
+        return maxCount;
+    }
+}
