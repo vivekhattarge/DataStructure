ce932566bef15b05d5ae370e34b4860b8e547ab0
Vivek.Hattarge <Vivek.Hattarge@gmail.com>
2021-04-03 22:21:24 +0530

    What :
    1. Problem to check whether given string are equal or not.
    
    Description : there will be a backspace character means
    if we found # character in string then don't consider immediate previous character for equality check.

---
+++ Data Structure/src/com/two/pointer/BackspaceCompare.java
@@ -0,0 +1,62 @@
+package com.two.pointer;
+
+public class BackspaceCompare {
+
+    public static void main(String[] args) {
+
+        String s1="xy#z";
+        String s2="xzz#";
+
+        System.out.println("Given strings are "+(areStringsEqual(s1,s2) ? "equal" : "not equal"));
+
+    }
+
+    private static boolean areStringsEqual(String s1, String s2) {
+
+        int index1 = s1.length()-1;
+        int index2 = s2.length()-1;
+
+        while(index1 >= 0 || index2 >= 0){
+
+            int i1 = getValidIndex(s1,index1);
+            int i2 = getValidIndex(s2,index2);
+
+            if(i1 < 0 && i2 < 0){
+                return true;
+            }
+
+            if(i1 < 0 || i2 < 0){
+                return false;
+            }
+
+            if(s1.charAt(i1) != s2.charAt(i2)){
+                return false;
+            }
+
+            index1 = i1-1;
+            index2 = i2-1;
+
+        }
+        return true;
+    }
+
+    private static int getValidIndex(String str, int index) {
+
+        int backspaceCount =0;
+        while(index >= 0){
+
+            if(str.charAt(index) == '#'){
+                backspaceCount++;
+            }else if(backspaceCount > 0){
+                backspaceCount--;
+            }else{
+                break;
+            }
+
+            index--;
+        }
+
+        return index;
+    }
+
+}
