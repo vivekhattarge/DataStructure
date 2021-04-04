2c77036aa666c882fe00d37230fac648fefa0dd8
Vivek.Hattarge <Vivek.Hattarge@gmail.com>
2021-04-04 12:39:38 +0530

    What :
    1. Detect whether linked list has cycle or not.

---
+++ Data Structure/src/com/slow/fast/LinkedListDetectCycle.java
@@ -0,0 +1,33 @@
+package com.slow.fast;
+
+public class LinkedListDetectCycle {
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        System.out.println("LinkedList has cycle: " + hasCycle(head));
+
+        head.next.next.next.next.next.next = head.next.next;
+        System.out.println("LinkedList has cycle: " + hasCycle(head));
+
+    }
+
+    private static boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ Data Structure/src/com/slow/fast/ListNode.java
@@ -0,0 +1,13 @@
+package com.slow.fast;
+
+public class ListNode {
+
+    int value = 0;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+
+
+}
