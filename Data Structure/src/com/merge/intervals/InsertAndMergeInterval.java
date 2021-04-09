21142015f4bb9a3ddb87db60045000d299d7e3a4
Vivek.Hattarge <Vivek.Hattarge@gmail.com>
2021-04-09 20:44:28 +0530

    What : 1. Program to insert interval in interval list.

---
+++ Data Structure/src/com/merge/intervals/InsertAndMergeInterval.java
@@ -0,0 +1,57 @@
+package com.merge.intervals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertAndMergeInterval {
+
+    public static void main(String[] args) {
+
+      /*  List<Interval> input = new ArrayList<Interval>();
+        input.add(new Interval(1, 3));
+        input.add(new Interval(5, 7));
+        input.add(new Interval(8, 12));
+        System.out.print("Intervals after inserting the new interval: ");
+        for (Interval interval : insert(input, new Interval(4, 6)))
+            System.out.print("[" + interval.start + "," + interval.end + "] ");
+        System.out.println();*/
+
+        List<Interval> input1 = new ArrayList<Interval>();
+        input1.add(new Interval(1, 3));
+        input1.add(new Interval(5, 7));
+        input1.add(new Interval(8, 12));
+        System.out.print("Intervals after inserting the new interval: ");
+        for (Interval interval : insert(input1, new Interval(4, 10)))
+            System.out.print("[" + interval.start + "," + interval.end + "] ");
+        System.out.println();
+
+
+    }
+
+    private static List<Interval> insert(List<Interval> inputList, Interval interval) {
+
+        List<Interval> mergedIntervals = new ArrayList<>();
+
+        int i=0;
+
+        while (i< inputList.size() && inputList.get(i).getEnd() < interval.getStart()){
+            mergedIntervals.add(new Interval(inputList.get(i).getStart(),inputList.get(i).getEnd()));
+            i++;
+        }
+
+        while(i<inputList.size() && inputList.get(i).getStart() <= interval.getEnd()){
+            interval.start = Math.min(interval.getStart(), inputList.get(i).getStart());
+            interval.end = Math.max(interval.getEnd(), inputList.get(i).getEnd());
+            i++;
+        }
+        mergedIntervals.add(new Interval(interval.start, interval.end));
+
+        while (i<inputList.size()){
+            mergedIntervals.add(new Interval(inputList.get(i).getStart(),inputList.get(i).getEnd()));
+            i++;
+        }
+
+        return mergedIntervals;
+    }
+
+}
