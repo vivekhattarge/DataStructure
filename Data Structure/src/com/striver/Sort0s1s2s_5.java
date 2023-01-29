0c47b957ae433b409967204135534af0767d9e04
vivek <vivek.hattarge@gmail.com>
2023-01-29 17:36:43 +0530

    buy and sell stock

--- Data Structure/src/com/striver/Sort0s1s2s_5.java
+++ Data Structure/src/com/striver/Sort0s1s2s_5.java
@@ -1,5 +1,21 @@
 package com.striver;
 
+/*
+Sort an array of 0s, 1s and 2s
+        Problem Statement: Given an array consisting of only 0s, 1s and 2s. Write a program to in-place sort the array without using inbuilt sort functions. ( Expected: Single pass-O(N) and constant space)
+
+        Example 1:
+
+        Input: nums = [2,0,2,1,1,0]
+        Output: [0,0,1,1,2,2]
+
+        Input: nums = [2,0,1]
+        Output: [0,1,2]
+
+        Input: nums = [0]
+        Input: nums = [0]
+*/
+
 public class Sort0s1s2s_5 {
     public static void main(String[] args) {
       //  int [] nums = {2,0,2,1,1,0};
