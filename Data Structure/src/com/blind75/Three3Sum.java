175f75d0bad0a2ca913b6a0163b33ef8ffcf91ed
vivek <vivek.hattarge@gmail.com>
2023-08-27 21:13:57 +0530

    15. 3Sum

---
+++ Data Structure/src/com/blind75/Three3Sum.java
@@ -0,0 +1,85 @@
+package com.blind75;
+
+/*
+15. 3Sum
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation:
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+* */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Three3Sum {
+
+    public static void main(String[] args) {
+        int [] nums = {-1,0,1,2,-1,-4};
+
+        List<List<Integer>> result = threeSum(nums);
+        System.out.println(result);
+    }
+
+    public static List<List<Integer>> threeSum(int[] arr) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
+        int n= arr.length;
+        for (int i = 0; i < n; i++) {
+            //remove duplicates:
+            if (i != 0 && arr[i] == arr[i - 1]) continue;
+
+            //moving 2 pointers:
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    //skip the duplicates:
+                    while (j < k && arr[j] == arr[j - 1]) j++;
+                    while (j < k && arr[k] == arr[k + 1]) k--;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+}
