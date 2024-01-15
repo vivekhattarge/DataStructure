29e2caad18b35a581c759824a83f39f89848d731
Vivek Hattarge <vivek.hattarge@gmail.com>
2024-01-15 20:21:28 +0530

    2225. Find Players With Zero or One Losses

--- Data Structure/src/com/daily/challenges/FindPlayersWithZeroOrOneLosses.java
+++ Data Structure/src/com/daily/challenges/FindPlayersWithZeroOrOneLosses.java
@@ -8,7 +8,6 @@ import java.util.*;
 You are given an integer array matches where matches[i] = [winneri, loseri] indicates that the player winneri defeated player loseri in a match.
 
 Return a list answer of size 2 where:
-
 answer[0] is a list of all players that have not lost any matches.
 answer[1] is a list of all players that have lost exactly one match.
 The values in the two lists should be returned in increasing order.
